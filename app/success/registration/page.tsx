0065f1c1505fd88a0cf58d84d674d46ca01e0a44
Atanu Mojumdar <91253222+AtanuMojumdar@users.noreply.github.com>
2024-03-16 21:42:34 +0530

    spam highlight

--- app/success/registration/page.tsx
+++ app/success/registration/page.tsx
@@ -1,13 +1,12 @@
 "use client"
-import Link from 'next/link';
 import React, { useEffect } from 'react'
 import {AlertDialogDemo} from '@/components/Contact'
 
 const Exit = () => {  
   return (
     <>
-    <div className='text-center text-5xl font-bold mt-14'>Thank You!</div>
-    <p className='text-center text-sm md:text-lg text-gray-400 mt-4'>Thanks for signing up. If you don&apos;t receive an email shortly, double check your spam box 🙂! or try again.</p>
+    <div className='text-center text-5xl font-bold mt-14'>Thank You<span className='text-sky-400'>!</span></div>
+    <p className='text-center text-sm md:text-lg text-gray-400 mt-4'>Thanks for signing up. If you don&apos;t receive an email shortly, <span className='text-sky-400'>double check your spam box</span> 🙂! or try again.</p>
     <div className='mx-auto w-fit'>
 
     <AlertDialogDemo/>
