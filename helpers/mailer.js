22d5847255ef04b334dcc4f8ca0227d51b0c2cae
Atanu Mojumdar <91253222+AtanuMojumdar@users.noreply.github.com>
2024-03-22 20:56:09 +0530

    poster changed

--- helpers/mailer.js
+++ helpers/mailer.js
@@ -20,7 +20,7 @@ async function sendMail(email){
         from : 'atanu19mojumdar@gmail.com',
         to: email,
         subject: "WebWizard Registration",
-        html: "<p>Thanks for signing up for WebWizard</p> <a href='https://docs.google.com/forms/d/e/1FAIpQLScuGijufEPYqpIBDBUbYUUESWWAzOKX2bN6NP9lIp6p_iBEzQ/viewform?usp=sf_link'>Workshop Registration Link</a> <br/> <br/> <p>Other links:</p> <a href='https://webwizard-omega.vercel.app/webwizardworkshopbrief.pdf'>Workshop Brief</a> &nbsp; </br> <a href='https://webwizard-omega.vercel.app/webwizardposter.png'>Workshop Poster</a> &nbsp; </br> <a href='https://webwizard-omega.vercel.app/howinternetworks.pdf'>Workshop Outline</a>  "
+        html: "<p>Thanks for signing up for WebWizard</p> <a href='https://docs.google.com/forms/d/e/1FAIpQLScuGijufEPYqpIBDBUbYUUESWWAzOKX2bN6NP9lIp6p_iBEzQ/viewform?usp=sf_link'>Workshop Registration Link</a> <br/> <br/> <p>Other links:</p> <a href='https://webwizard-omega.vercel.app/webwizardworkshopbrief.pdf'>Workshop Brief</a> &nbsp; </br> <a href='https://webwizard-omega.vercel.app/webwizard_poster.png'>Workshop Poster</a> &nbsp; </br> <a href='https://webwizard-omega.vercel.app/howinternetworks.pdf'>Workshop Outline</a>  "
       }
     const mailresponse = await transporter.sendMail(mailOptions);
     return mailresponse;
